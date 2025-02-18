f8a2132a9f6c71a410006019becc1744b9ad8e66
tjkoury <tj@digitalarsenal.io>
2025-02-18 15:28:05 -0500

    deploy

--- src/lib/SpaceObjects/lib/models.ts
+++ src/lib/SpaceObjects/lib/models.ts
@@ -3,7 +3,7 @@ import issModel from "@/assets/models/ISS_stationary.glb?url";
 import onewebModel from "@/assets/models/oneweb.glb?url";
 
 export default (name: any) => {
-    const sname = name.toString()?.toLowerCase();
+    const sname = name?.toString()?.toLowerCase();
     if (sname.includes("starlink")) {
         return starlinkModel;
     } else if (sname.includes("zarya")) {
